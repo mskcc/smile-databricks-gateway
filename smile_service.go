c4aabd7c37b4dc7f45a078690c083133f50ce649
Benjamin Gross <benjamin.gross@gmail.com>
2024-12-04 15:49:05 -0500

    Clean up of trace messages

--- smile_service.go
+++ smile_service.go
@@ -24,11 +24,13 @@ type SmileService struct {
 type RequestAdapter struct {
 	Requests []SmileRequest
 	Msg      *nm.Msg
+	SpanCtx  context.Context
 }
 
 type SampleAdapter struct {
 	Samples []SmileSample
 	Msg     *nm.Msg
+	SpanCtx context.Context
 }
 
 const (
@@ -45,35 +47,33 @@ func NewSmileService(url, certPath, keyPath, consumer, password string, awsS3Ser
 }
 
 const (
-	runMsg                 = "Entering smile_service.Run() loop iteration"
-	runSubscribeMsg        = "Subscribing to SMILE message topics"
-	runLoopSpanMsg         = "Entering select on incoming SMILE messages"
-	newReqInsertMsg        = "Attempting to insert new request in databricks"
+	newReqS3WriteMsg       = "Attempting to write new request into S3 bucket"
 	IGORequestIdKey        = "IGO Request ID"
-	newReqDBInsertErrMsg   = "Error inserting new request into Databricks"
-	newReqDBInsertSucMsg   = "Successfully inserted new request into Databricks"
-	newSampDBInsertErrMsg  = "Error inserting new samples into Databricks"
-	newSampDBInsertSucMsg  = "Successfully inserted new samples into Databricks"
-	updateRequestMsg       = "Attempting to update a request in databricks"
-	upReqDBUpdateErrMsg    = "Error updating request in Databricks"
-	upReqDBUpdateSucMsg    = "Successfully updated request into Databricks"
+	newReqS3WriteErrMsg    = "Error writing new request into S3 bucket"
+	newReqS3WriteSucMsg    = "Successfully wrote new request into S3 bucket"
+	succProcessedNewReqMsg = "Successfully processed new request: %d"
+	newSampleS3WriteErrMsg = "Error writing new sample into S3 bucket"
+	newSampleS3WriteSucMsg = "Successfully wrote new sample into S3 bucket"
+	NumSamplesWrittenKey   = "Num Samples Written"
+	updateReqS3WriteMsg    = "Attempting to update a request in an S3 bucket"
+	upReqS3WriteErrMsg     = "Error updating request in an S3 bucket"
+	upReqS3WriteSucMsg     = "Successfully updated request in an S3 bucket"
+	succProcessedUpReqMsg  = "Successfully processed request update: %d"
 	SampleNameKey          = "Sample Name"
-	updateSampleMsg        = "Attempting to update a sample in databricks"
-	upSampleDBUpdateErrMsg = "Error updating sample in Databricks"
-	upSampleDBUpdateSucMsg = "Successfully updated sample into Databricks"
+	updateSampleS3WriteMsg = "Attempting to update a sample in an S3 bucket"
+	upSampleS3WriteErrMsg  = "Error updating sample in an S3 bucket"
+	upSampleS3WriteSucMsg  = "Successfully updated a sample in an S3 bucket"
+	succProcessedUpSampMsg = "Successfully processed sample update: %d"
 	execDLTPipelineErrMsg  = "Unsuccessfully executed DLT pipeline"
 	execDLTPipelineSucMsg  = "Successfully executed DLT pipeline"
 )
 
 func (ss *SmileService) Run(ctx context.Context, consumer, subject, newRequestFilter, updateRequestFilter, updateSampleFilter string, tracer trace.Tracer) error {
 
-	runCtx, runSpan := tracer.Start(ctx, runMsg)
-
 	newRequestChan := make(chan RequestAdapter, requestBufSize)
 	updateRequestChan := make(chan RequestAdapter, requestBufSize)
 	updateSampleChan := make(chan SampleAdapter, sampleBufSize)
-	runSpan.AddEvent(runSubscribeMsg)
-	err := ss.subscribeToService(runCtx, newRequestChan, updateRequestChan, updateSampleChan,
+	err := ss.subscribeToService(ctx, newRequestChan, updateRequestChan, updateSampleChan,
 		consumer, subject, newRequestFilter, updateRequestFilter, updateSampleFilter, tracer)
 	if err != nil {
 		return err
@@ -83,11 +83,9 @@ func (ss *SmileService) Run(ctx context.Context, consumer, subject, newRequestFi
 	var urwg sync.WaitGroup
 	var uswg sync.WaitGroup
 	for {
-		selectCtx, runLoopSpan := tracer.Start(runCtx, runLoopSpanMsg)
-
 		select {
 		case ra := <-newRequestChan:
-			_, nrSpan := tracer.Start(selectCtx, newReqInsertMsg)
+			nrCtx, nrSpan := tracer.Start(ra.SpanCtx, newReqS3WriteMsg)
 			nrSpan.SetAttributes(attribute.String(IGORequestIdKey, ra.Requests[0].IgoRequestID))
 			nrwg.Add(1)
 			go func() {
@@ -97,45 +95,51 @@ func (ss *SmileService) Run(ctx context.Context, consumer, subject, newRequestFi
 				samples := ra.Requests[0].Samples
 				ra.Requests[0].Samples = nil
 				err := ss.awsS3Service.PutRequest(filename, ra.Requests[0])
-				if handleError(err, newReqDBInsertErrMsg, nrSpan) {
+				if handleError(err, newReqS3WriteErrMsg, nrSpan) {
 					return
 				}
 				for _, sample := range samples {
 					filename := fmt.Sprintf("%s_sample.json", sample.SampleName)
 					err := ss.awsS3Service.PutSample(filename, sample)
-					if handleError(err, newReqDBInsertErrMsg, nrSpan) {
+					if handleError(err, newSampleS3WriteErrMsg, nrSpan) {
 						return
 					}
+					nrSpan.AddEvent(newSampleS3WriteSucMsg, trace.WithAttributes(attribute.String(SampleNameKey, sample.SampleName)))
 				}
-				nrSpan.AddEvent(newReqDBInsertSucMsg)
+				nrSpan.SetAttributes(attribute.Int(NumSamplesWrittenKey, len(samples)))
+				nrSpan.AddEvent(newReqS3WriteSucMsg, trace.WithAttributes(attribute.String(IGORequestIdKey, ra.Requests[0].IgoRequestID)))
 				ra.Msg.ProviderMsg.Ack()
-				err = ss.databricksService.ExecutePipeline(selectCtx)
+				err = ss.databricksService.ExecutePipeline(nrCtx)
 				if handleError(err, execDLTPipelineErrMsg, nrSpan) {
 					return
 				}
 				nrSpan.AddEvent(execDLTPipelineSucMsg)
+				nrSpan.SetStatus(codes.Ok, fmt.Sprintf(succProcessedNewReqMsg, ra.Requests[0].IgoRequestID))
+				nrSpan.End()
 			}()
 		case ra := <-updateRequestChan:
-			_, urSpan := tracer.Start(selectCtx, updateRequestMsg)
+			urCtx, urSpan := tracer.Start(ra.SpanCtx, updateReqS3WriteMsg)
 			urSpan.SetAttributes(attribute.String(IGORequestIdKey, ra.Requests[0].IgoRequestID))
 			urwg.Add(1)
 			go func() {
 				defer urwg.Done()
 				filename := fmt.Sprintf("%s_request.json", ra.Requests[0].IgoRequestID)
 				err := ss.awsS3Service.PutRequest(filename, ra.Requests[0])
-				if handleError(err, upReqDBUpdateErrMsg, urSpan) {
+				if handleError(err, upReqS3WriteErrMsg, urSpan) {
 					return
 				}
-				urSpan.AddEvent(upReqDBUpdateSucMsg)
+				urSpan.AddEvent(upReqS3WriteSucMsg)
 				ra.Msg.ProviderMsg.Ack()
-				err = ss.databricksService.ExecutePipeline(selectCtx)
+				err = ss.databricksService.ExecutePipeline(urCtx)
 				if handleError(err, execDLTPipelineErrMsg, urSpan) {
 					return
 				}
 				urSpan.AddEvent(execDLTPipelineSucMsg)
+				urSpan.SetStatus(codes.Ok, fmt.Sprintf(succProcessedUpReqMsg, ra.Requests[0].IgoRequestID))
+				urSpan.End()
 			}()
 		case sa := <-updateSampleChan:
-			_, usSpan := tracer.Start(selectCtx, updateSampleMsg)
+			usCtx, usSpan := tracer.Start(sa.SpanCtx, updateSampleS3WriteMsg)
 			usSpan.SetAttributes(attribute.String(IGORequestIdKey, sa.Samples[0].AdditionalProperties.IgoRequestID))
 			usSpan.SetAttributes(attribute.String(SampleNameKey, sa.Samples[0].SampleName))
 			uswg.Add(1)
@@ -143,16 +147,18 @@ func (ss *SmileService) Run(ctx context.Context, consumer, subject, newRequestFi
 				defer uswg.Done()
 				filename := fmt.Sprintf("%s_sample.json", sa.Samples[0].SampleName)
 				err := ss.awsS3Service.PutSample(filename, sa.Samples[0])
-				if handleError(err, upSampleDBUpdateErrMsg, usSpan) {
+				if handleError(err, upSampleS3WriteErrMsg, usSpan) {
 					return
 				}
-				usSpan.AddEvent(upSampleDBUpdateSucMsg)
+				usSpan.AddEvent(upSampleS3WriteSucMsg)
 				sa.Msg.ProviderMsg.Ack()
-				err = ss.databricksService.ExecutePipeline(selectCtx)
+				err = ss.databricksService.ExecutePipeline(usCtx)
 				if handleError(err, execDLTPipelineErrMsg, usSpan) {
 					return
 				}
 				usSpan.AddEvent(execDLTPipelineSucMsg)
+				usSpan.SetStatus(codes.Ok, fmt.Sprintf(succProcessedUpSampMsg, sa.Samples[0].SampleName))
+				usSpan.End()
 			}()
 		case <-ctx.Done():
 			log.Println("Context canceled, returning...")
@@ -160,10 +166,8 @@ func (ss *SmileService) Run(ctx context.Context, consumer, subject, newRequestFi
 			urwg.Wait()
 			uswg.Wait()
 			ss.natsMessaging.Shutdown()
-			runLoopSpan.End()
 			return nil
 		}
-		runLoopSpan.End()
 	}
 }
 
@@ -174,11 +178,14 @@ const (
 
 	incomingUpReqMsg      = "Updated incoming request"
 	processingUpReqErrMsg = "Error unmarshaling updated incoming request"
-	processingUpReqSucMsg = "Successfully unmarshaled updated incoming request"
+	processingUpReqSucMsg = "Successfully received and unmarshaled updated incoming request"
 
 	incomingUpSampMsg      = "Updated incoming sample"
 	processingUpSampErrMsg = "Error unmarshaling updated incoming sample"
-	processingUpSampSucMsg = "Successfully unmarshaled updated incoming sample"
+	processingUpSampSucMsg = "Successfully received and unmarshaled updated incoming sample"
+
+	handingOffRequestToRunLoopMsg = "Handing off request for writing to S3 bucket connected to Databricks"
+	handingOffSampleToRunLoopMsg  = "Handing off sample for writing to an S3 bucket connected to Databricks"
 )
 
 func (ss *SmileService) subscribeToService(ctx context.Context, newRequestCh, upRequestCh chan RequestAdapter, upSampleCh chan SampleAdapter,
@@ -186,32 +193,35 @@ func (ss *SmileService) subscribeToService(ctx context.Context, newRequestCh, up
 	err := ss.natsMessaging.Subscribe(consumer, subject, func(m *nm.Msg) {
 		switch {
 		case m.Subject == newRequestFilter:
-			_, nrSpan := tracer.Start(ctx, incomingNewReqMsg)
+			subscribeCtx, nrSpan := tracer.Start(ctx, incomingNewReqMsg)
 			nr, err := unMarshal[SmileRequest](string(m.Data))
 			if handleError(err, processingNewReqErrMsg, nrSpan) {
 				break
 			}
-			nrSpan.AddEvent(processingNewReqSucMsg)
+			nrSpan.AddEvent(processingNewReqSucMsg, trace.WithAttributes(attribute.String(IGORequestIdKey, nr.IgoRequestID)))
+			nrSpan.AddEvent(handingOffRequestToRunLoopMsg, trace.WithAttributes(attribute.String(IGORequestIdKey, nr.IgoRequestID)))
 			nrSpan.End()
-			newRequestCh <- RequestAdapter{[]SmileRequest{nr}, m}
+			newRequestCh <- RequestAdapter{[]SmileRequest{nr}, m, subscribeCtx}
 		case m.Subject == updateRequestFilter:
-			_, urSpan := tracer.Start(ctx, incomingUpReqMsg)
+			subscribeCtx, urSpan := tracer.Start(ctx, incomingUpReqMsg)
 			ru, err := unMarshal[[]SmileRequest](string(m.Data))
 			if handleError(err, processingUpReqErrMsg, urSpan) {
 				break
 			}
-			urSpan.AddEvent(processingUpReqSucMsg)
+			urSpan.AddEvent(processingUpReqSucMsg, trace.WithAttributes(attribute.String(IGORequestIdKey, ru[0].IgoRequestID)))
+			urSpan.AddEvent(handingOffRequestToRunLoopMsg, trace.WithAttributes(attribute.String(IGORequestIdKey, ru[0].IgoRequestID)))
 			urSpan.End()
-			upRequestCh <- RequestAdapter{ru, m}
+			upRequestCh <- RequestAdapter{ru, m, subscribeCtx}
 		case m.Subject == updateSampleFilter:
-			_, usSpan := tracer.Start(ctx, incomingUpSampMsg)
+			subscribeCtx, usSpan := tracer.Start(ctx, incomingUpSampMsg)
 			su, err := unMarshal[[]SmileSample](string(m.Data))
 			if handleError(err, processingUpSampErrMsg, usSpan) {
 				break
 			}
-			usSpan.AddEvent(processingUpSampSucMsg)
+			usSpan.AddEvent(processingUpSampSucMsg, trace.WithAttributes(attribute.String(SampleNameKey, su[0].SampleName)))
+			usSpan.AddEvent(handingOffSampleToRunLoopMsg, trace.WithAttributes(attribute.String(SampleNameKey, su[0].SampleName)))
 			usSpan.End()
-			upSampleCh <- SampleAdapter{su, m}
+			upSampleCh <- SampleAdapter{su, m, subscribeCtx}
 		default:
 			// not interested in message, Ack it so we don't get it again
 			m.ProviderMsg.Ack()
